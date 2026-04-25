c978c2359f9f86a458c93e703bf2bf1e2d7190df
dev778g-me <sanji778g@gmail.com>
2026-04-25 16:59:53 +0530

    Add - added minor things

---
+++ composeApp/src/iosMain/kotlin/com/dev/kore/Platform.ios.kt
@@ -0,0 +1,8 @@
+package com.dev.kore
+
+import platform.UIKit.UIDevice
+
+class IOSPlatform: Platform {
+    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+}
+
