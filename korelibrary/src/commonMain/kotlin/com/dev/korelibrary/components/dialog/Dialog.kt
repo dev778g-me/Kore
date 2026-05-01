c1716ef5f12b6c7b26b837fe1cfd5c4ca57917de
dev778g-me <sanji778g@gmail.com>
2026-05-01 11:49:53 +0530

    Refactor - refactored alertDialog

--- korelibrary/src/commonMain/kotlin/com/dev/korelibrary/components/dialog/Dialog.kt
+++ korelibrary/src/commonMain/kotlin/com/dev/korelibrary/components/dialog/Dialog.kt
@@ -1,6 +1,8 @@
 package com.dev.korelibrary.components.dialog
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,25 +20,50 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.dev.korelibrary.themes.KoreTheme
+import com.dev.korelibrary.themes.LocalContentColor
 import com.dev.korelibrary.themes.LocalTextStyle
 
-
+/**
+ * Alert Dialog is a popup that overlays main content to display urgent message or require a decision before user proceeds.
+ * @param onDismissRequest the callback which will get called when the user tries to dismiss the dialog by clicking outside of it or pressing back button.
+ * @param modifier the modifier to apply to Alert Dialog
+ * @param title the title of the Dialog usually heading which conveys alert message to user .
+ * @param icon the icon of the Dialog appears on top of title which usually conveys what type of action user will be performing .
+ * @param description
+ * @param dismissButton it is usually the action which you don't wanna execute by user .
+ * @param confirmButton it is usually the action which you want user to execute ..
+ * @param dialogShape the shape of the dialog Container .
+ * @param dialogColors the colors of the dialogs . use [DialogDefaults.alertDialogColors] to customize .
+ * @param border the border of the dialog [BorderStroke]
+ * @param elevation the elevation value of the dialog [Dp]
+ * @param dialogActionArrangement the horizontal arrangement of the actions(i.e. confirm button , dismiss button) .
+ * @param dialogProperties the properties of the dialog .
+ * @param dialogSpacing the spacing between the elements of the dialog . Use [DialogDefaults.defaultDialogSpacing] to customize .
+ * @param contentPadding the padding of the dialog content .
+ */
 @Composable
 fun AlertDialog(
     onDismissRequest: () -> Unit,
     modifier: Modifier = Modifier,
-    title : @Composable () -> Unit,
-    description : @Composable () -> Unit = {},
-    dialogShape : Shape = DialogDefaults.defaultDialogShape,
+    title: @Composable () -> Unit,
+    icon : (@Composable () -> Unit)? = null,
+    description: (@Composable () -> Unit)? = null,
+    dismissButton: (@Composable () -> Unit)? = null,
+    confirmButton: @Composable () -> Unit,
+    dialogShape: Shape = DialogDefaults.defaultDialogShape,
     dialogColors: DialogColors = DialogDefaults.alertDialogColors(),
-    dismissButton : @Composable () -> Unit ={},
-    confirmButton : @Composable () -> Unit ={},
+    border : BorderStroke? = null,
+    elevation: Dp = DialogDefaults.defaultDialogElevation,
+    dialogActionArrangement: Arrangement.Horizontal = DialogDefaults.defaultActionArrangement,
     dialogProperties: DialogProperties = DialogDefaults.defaultDialogProperties,
-    dialogPadding : PaddingValues = DialogDefaults.defaultDialogPaddingValues
+    dialogSpacing: DialogSpacing = DialogDefaults.defaultDialogSpacing(),
+    contentPadding: PaddingValues = DialogDefaults.defaultDialogPaddingValues
 ) {
     Dialog(
         onDismissRequest = {
@@ -47,16 +74,28 @@ fun AlertDialog(
         Box(
             modifier = modifier
                 .fillMaxWidth()
-                .shadow(8.dp, dialogShape)
+                .shadow(elevation, dialogShape)
                 .background(
                     color = dialogColors.containerColor,
                     shape = dialogShape
-                )
+                ).then(if (border != null) Modifier.border(border, dialogShape) else Modifier)
                 .padding(
-                    paddingValues = dialogPadding
+                    paddingValues = contentPadding
                 )
         ) {
             Column() {
+
+                icon?.let {
+                    CompositionLocalProvider(
+                        value = LocalContentColor provides KoreTheme.colorScheme.primary
+                    ){
+                        it()
+                        Spacer(
+                            modifier = Modifier.height(dialogSpacing.afterIconSpacing)
+                        )
+                    }
+                }
+
                 // top heading
                 CompositionLocalProvider(
                     LocalTextStyle provides KoreTheme.typography.title1.copy(
@@ -66,33 +105,38 @@ fun AlertDialog(
                     title()
                 }
                 Spacer(
-                    modifier = Modifier.height(8.dp)
+                    modifier = Modifier.height(dialogSpacing.afterTitleSpacing)
                 )
                 // middle part for explaining the intent
-               CompositionLocalProvider(
-                   LocalTextStyle provides KoreTheme.typography.title3.copy(
-                       color = dialogColors.descriptionTextColor
-                   )
-               ) {
-                   description()
+               description?.let {
+                   CompositionLocalProvider(
+                       LocalTextStyle provides KoreTheme.typography.title3.copy(
+                           color = dialogColors.descriptionTextColor
+                       )
+                   ) {
+                       it()
+                       Spacer(
+                           modifier = Modifier.height(dialogSpacing.afterDescriptionSpacing)
+                       )
+                   }
                }
 
-                Spacer(
-                    modifier = Modifier.height(16.dp)
-                )
+
 
                 // row for actions
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.End
+                    modifier = Modifier.fillMaxWidth().semantics{
+                        false
+                    },
+                    horizontalArrangement = dialogActionArrangement
                 ) {
                     //less likely task
-                   dismissButton.let {
-                       dismissButton()
-                       Spacer(
-                           modifier = Modifier.width(8.dp)
-                       )
-                   }
+                    dismissButton?.let {
+                        dismissButton()
+                        Spacer(
+                            modifier = Modifier.width(dialogSpacing.buttonSpacing)
+                        )
+                    }
                     // primary task y wanna execute
                     confirmButton()
 
@@ -109,13 +153,28 @@ object DialogDefaults{
 
     val defaultDialogShape : Shape
         @Composable get() = KoreTheme.shapes.lg
-    val defaultDialogPaddingValues : PaddingValues = PaddingValues(24.dp)
+    val defaultDialogPaddingValues : PaddingValues
+        @Composable get() = PaddingValues(KoreTheme.sizes.lg)
+
+
+    val defaultDialogElevation : Dp = 8.dp
 
     val defaultDialogProperties : DialogProperties = DialogProperties(
         usePlatformDefaultWidth = true,
         dismissOnBackPress = true,
-        dismissOnClickOutside = true
+        dismissOnClickOutside = true,
     )
+
+    @Composable
+    fun defaultDialogSpacing() : DialogSpacing = DialogSpacing(
+        afterIconSpacing = KoreTheme.sizes.xs,
+        afterTitleSpacing = KoreTheme.sizes.xs,
+        afterDescriptionSpacing = KoreTheme.sizes.sm,
+        buttonSpacing = KoreTheme.sizes.xs
+    )
+
+
+    val defaultActionArrangement : Arrangement.Horizontal = Arrangement.End
 
 
 
@@ -144,6 +203,13 @@ data class DialogColors(
     val borderColor : Color
 )
 
+@Immutable
+data class DialogSpacing(
+    val afterIconSpacing: Dp,
+    val afterTitleSpacing: Dp,
+    val afterDescriptionSpacing: Dp,
+    val buttonSpacing: Dp
+)
 
 
 
