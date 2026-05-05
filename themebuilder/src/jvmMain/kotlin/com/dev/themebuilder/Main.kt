df86ca10ed1792343c8990d1f9419259a187026b
dev778g-me <sanji778g@gmail.com>
2026-05-05 11:50:16 +0530

    CI - ci for publishing

---
+++ themebuilder/src/jvmMain/kotlin/com/dev/themebuilder/Main.kt
@@ -0,0 +1,25 @@
+package com.dev.themebuilder
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import com.dev.themebuilder.ui.ThemeApp
+import kore.themebuilder.generated.resources.Res
+import kore.themebuilder.generated.resources.icon_desktop
+import org.jetbrains.compose.resources.painterResource
+
+fun main() = application {
+    val appIcon = painterResource(resource = Res.drawable.icon_desktop)
+    Window(
+        state = rememberWindowState(
+           placement = WindowPlacement.Maximized
+        ),
+        icon = appIcon,
+        onCloseRequest = ::exitApplication,
+        title = "Kore",
+    ) {
+
+        ThemeApp()
+    }
+}
