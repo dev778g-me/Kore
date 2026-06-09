54578c39e0aed50074ce8424d2f7cece46a0c2e5
dev778g-me <sanji778g@gmail.com>
2026-06-09 16:12:39 +0530

    Add - added new Modifier extensions

--- kore/src/commonMain/kotlin/com/dev/kore/utilities/modifiers/modifierExtensions.kt
+++ kore/src/commonMain/kotlin/com/dev/kore/utilities/modifiers/modifierExtensions.kt
@@ -1,18 +1,17 @@
 package com.dev.kore.utilities.modifiers
 
-import androidx.compose.animation.Animatable
 import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -25,26 +24,27 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.graphics.CompositingStrategy
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.CompositingStrategy
 import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.PointMode
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
@@ -58,15 +58,21 @@ import kotlin.math.roundToInt
 fun Modifier.scaleClickable(
     pressedScale: Float = 0.95f,
     onClick: () -> Unit,
-    interactionSource: MutableInteractionSource ? =null
+    role: Role? = null,
+    animationSpec: AnimationSpec<Float> ? = null,
+    interactionSource: MutableInteractionSource ? = null,
 ): Modifier = composed {
 
-    val interactionSource = remember { interactionSource ?: MutableInteractionSource() }
+    val scopedInteractionSource = remember { interactionSource ?: MutableInteractionSource() }
 
-    val isPressed by interactionSource.collectIsPressedAsState()
+    val isPressed by scopedInteractionSource.collectIsPressedAsState()
 
     val animatedScale by animateFloatAsState(
         targetValue = if (isPressed) pressedScale else 1f,
+        animationSpec = animationSpec ?: spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        ),
         label = "pressEffect"
     )
 
@@ -76,7 +82,8 @@ fun Modifier.scaleClickable(
             scaleY = animatedScale
         }
         .clickable(
-            interactionSource = interactionSource,
+            role = role,
+            interactionSource = scopedInteractionSource,
             indication = null,
             onClick = onClick
         )
@@ -89,26 +96,43 @@ fun Modifier.blueprintGrid(
     gridSize: Dp = 16.dp,
     dotRadius: Dp = 1.dp,
     dotColor: Color = Color.Black
-): Modifier = this.drawBehind {
+): Modifier = this.drawWithCache {
     val step = gridSize.toPx()
     val radius = dotRadius.toPx()
 
-    if (step <= 0f) return@drawBehind
+    val points = mutableListOf<Offset>()
+    if (step > 0f) {
 
-    val columns = ceil(size.width / step).toInt()
-    val rows = ceil(size.height / step).toInt()
+        val offsetX = (size.width % step) / 2f
+        val offsetY = (size.height % step) / 2f
 
-    for (x in 0..columns) {
-        for (y in 0..rows) {
-            drawCircle(
+        val columns = (size.width / step).toInt()
+        val rows = (size.height / step).toInt()
+
+        for (x in 0..columns) {
+            for (y in 0..rows) {
+                points.add(
+                    Offset(
+                        x = offsetX + (x * step),
+                        y = offsetY + (y * step)
+                    )
+                )
+            }
+        }
+    }
+
+    onDrawBehind {
+        if (points.isNotEmpty()) {
+            drawPoints(
+                points = points,
+                pointMode = PointMode.Points,
                 color = dotColor,
-                radius = radius,
-                center = Offset(x * step, y * step)
+                strokeWidth = radius * 2,
+                cap = StrokeCap.Round
             )
         }
     }
 }
-
 
 
 /**
@@ -142,7 +166,6 @@ fun Modifier.magneticPull(pullStrength: Float = 20f): Modifier = composed {
             ) { change, dragAmount ->
                 change.consume()
                 scope.launch {
-
                     offsetX.snapTo(offsetX.value + (dragAmount.x / pullStrength))
                     offsetY.snapTo(offsetY.value + (dragAmount.y / pullStrength))
                 }
@@ -154,6 +177,7 @@ fun Modifier.magneticPull(pullStrength: Float = 20f): Modifier = composed {
 fun Modifier.noRippleClickable(
     onClick : () -> Unit,
     enabled : Boolean = true,
+    role: Role ? = null,
     interactionSource: MutableInteractionSource ? = null
 ) : Modifier = composed{
 
@@ -161,6 +185,7 @@ fun Modifier.noRippleClickable(
 
     this
         .clickable(
+            role = role ,
             onClick = onClick,
             interactionSource = scopedInteractionSource,
             indication = null,
@@ -216,7 +241,7 @@ fun Modifier.shimmer(
 }
 
 
-fun Modifier.minimumInteractiveComponentSize(): Modifier = layout { measurable, constraints ->
+private fun Modifier.minimumInteractiveComponentSize(): Modifier = layout { measurable, constraints ->
     val placeable = measurable.measure(constraints)
 
     val minWidthPx = 48.dp.roundToPx()
@@ -233,7 +258,12 @@ fun Modifier.minimumInteractiveComponentSize(): Modifier = layout { measurable, 
     }
 }
 
-
+/**
+A utility modifier that clears the active focus state when the component is tapped.
+This modifier is incredibly useful for form screens and input-heavy UIs.
+By applying it to a parent container (like a Box or Column),
+you can allow users to dismiss the software keyboard and drop focus from a TextField simply by tapping anywhere on the empty background space.
+*/
 fun Modifier.clearFocusOnTap(): Modifier = composed {
     val focusManager = LocalFocusManager.current
     this.pointerInput(Unit) {
@@ -305,7 +335,7 @@ fun Modifier.horizontalFadingEdges(
 /**
  * Modifies the element to add a dashed border along the outline of a specified shape.
  * Unlike a standard  border, this modifier draws dashes.
- * @param color The color of the dash border.
+ * @param color the color of the dash border.
  * @param thickness the thickness of the border .
  * @param dashLength The length of each individual dash [Dp]. Defaults to 6.dp.
  * @param gapLength The visual empty space between the edges of consecutive dots [Dp]. Defaults to 4.dp.
@@ -342,6 +372,55 @@ fun Modifier.dashedBorder(
         drawContent()
         drawPath(
             color = color,
+            path = borderPath,
+            style = dashStroke
+        )
+
+
+    }
+}
+
+
+/**
+ * Modifies the element to add a dashed border along the outline of a specified shape.
+ * Unlike a standard  border, this modifier draws dashes.
+ * @param brush the gradient color of the dash border.
+ * @param thickness the thickness of the border .
+ * @param dashLength The length of each individual dash [Dp]. Defaults to 6.dp.
+ * @param gapLength The visual empty space between the edges of consecutive dots [Dp]. Defaults to 4.dp.
+ * @param shape The shape of the border to draw the dots along (e.g., RectangleShape, RoundedCornerShape).
+ */
+fun Modifier.dashedBorder(
+    brush: Brush,
+    thickness: Dp = 1.dp,
+    dashLength: Dp = 6.dp,
+    gapLength: Dp = 4.dp,
+    shape: Shape
+): Modifier = this.drawWithCache {
+    val strokeWidthPx = thickness.toPx()
+    val dashLengthPx = dashLength.toPx()
+    val gapLengthPx = gapLength.toPx()
+
+    val outline = shape.createOutline(size, layoutDirection, this)
+
+    val borderPath = Path().apply {
+        when (outline) {
+            is Outline.Rounded -> addRoundRect(roundRect = outline.roundRect)
+            is Outline.Rectangle -> addRect(rect = outline.rect)
+            is Outline.Generic -> addPath(path = outline.path)
+        }
+    }
+    val dashStroke = Stroke(
+        width = strokeWidthPx,
+        pathEffect = PathEffect.dashPathEffect(
+            intervals = floatArrayOf(dashLengthPx, gapLengthPx),
+            phase = 0f
+        )
+    )
+    onDrawWithContent {
+        drawContent()
+        drawPath(
+            brush = brush,
             path = borderPath,
             style = dashStroke
         )
@@ -398,4 +477,52 @@ fun Modifier.dotBorder(
     }
 }
 
+
+/**
+ * Modifies the element to add a dotted border along the outline of a specified shape.
+ * Unlike a standard  border, this modifier draws perfect circles for each dot.
+ * The total thickness of the border will be twice the [dotRadius] (the diameter).
+ * @param brush the gradient color of the dotted border.
+ * @param dotRadius The radius of each individual dot [Dp]. Defaults to 2.dp.
+ * @param gapLength The visual empty space between the edges of consecutive dots [Dp]. Defaults to 4.dp.
+ * @param shape The shape of the border to draw the dots along (e.g., RectangleShape, RoundedCornerShape).
+ */
+fun Modifier.dotBorder(
+    brush: Brush,
+    dotRadius: Dp = 1.dp,
+    gapLength: Dp = 4.dp,
+    shape: Shape
+): Modifier = this.drawWithCache {
+    val diameterPx = (dotRadius * 2).toPx()
+    val gapLengthPx = gapLength.toPx()
+
+    val outline = shape.createOutline(size, layoutDirection, this)
+
+    val borderPath = Path().apply {
+        when (outline) {
+            is Outline.Rounded -> addRoundRect(roundRect = outline.roundRect)
+            is Outline.Rectangle -> addRect(rect = outline.rect)
+            is Outline.Generic -> addPath(path = outline.path)
+        }
+    }
+
+    val dashStroke = Stroke(
+        join = StrokeJoin.Miter,
+        width = diameterPx,
+        cap = StrokeCap.Round,
+        pathEffect = PathEffect.dashPathEffect(
+            intervals = floatArrayOf(0f, diameterPx + gapLengthPx),
+            phase = 0f
+        )
+    )
+
+    onDrawWithContent {
+        drawContent()
+        drawPath(
+            brush = brush,
+            path = borderPath,
+            style = dashStroke
+        )
+    }
+}
 
